e179139049fe744445b02e2d0c61ff75f74be857
YoussefMekkaoui <youssef.mekkaoui@whaleva.com>
2024-10-19 00:22:48 +0200

    ss

--- frontend/src/services/apiServices.js
+++ frontend/src/services/apiServices.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export const loginUser = async (username, password) => {
   try {
-    const response = await fetch(`${API_URL}/auth/login`, {
+    const response = await fetch(`/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
