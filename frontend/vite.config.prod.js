b1614403ca4ab84fd97c7eff8c2e0bb6110b24e9
YoussefMekkaoui <youssef.mekkaoui@whaleva.com>
2024-10-19 00:29:53 +0200

    ddsq

--- frontend/vite.config.prod.js
+++ frontend/vite.config.prod.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.NODE_ENV': JSON.stringify('development')
+    'process.env.NODE_ENV': JSON.stringify('production')
   },
   server: {
-    port: 3000,
+    port: 5000,
   },
   base: '/',
 });
