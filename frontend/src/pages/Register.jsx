29bd00dbf5177527fe8b2e0521524c82d6bcfe23
YoussefMekkaoui <youssef.mekkaoui@whaleva.com>
2025-02-01 14:53:06 +0100

    REGISTER

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -286,7 +286,7 @@ const Register = () => {
         other_sector: formData.otherSector || null,
         id_country: selectedCountry ? selectedCountry.id_country : null,
         id_country_headoffice: selectedHeadOfficeCountry ? selectedHeadOfficeCountry.id_country : null,
-        other_legal_form : otherCompanyCategory,
+        other_legal_form : formData.otherCompanyCategory,
         statut_flag: 1,
         idlogin: 1,
         billed_cust_name: formData.billed_cust_name,
