7967298cdd462559c79bcab1a7894f981f50fa21
Ahmed <mustapha.ahmedmohamedali@gmail.com>
2025-02-26 17:29:10 +0100

    css remarque mohamed 26 2

---
+++ frontend/src/pages/DashboardOperateur.jsx
@@ -0,0 +1,12 @@
+// EmptyPage.jsx
+import React from 'react';
+
+const EmptyPage = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      {/* Page vide */}
+    </div>
+  );
+};
+
+export default EmptyPage;
